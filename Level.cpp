57659c8125f46effbf6a16658cc992c9816259ec
IONA O'DONNELL <1614040@fife.ac.uk>
2018-11-21 14:15:46 +0000

    Set up framework

---
+++ Level.cpp
@@ -0,0 +1,126 @@
+// Project Includes
+#include "Level.h"
+
+
+// Library Includes
+#include <iostream>
+#include <fstream>
+
+Level::Level()
+	: m_currentLevel(0)
+
+{
+	LoadLevel(1);
+}
+
+
+void Level::Draw(sf::RenderTarget& _target)
+{
+	// Create and update
+	sf::View camera = _target.getDefaultView();
+	//TODO: Adjust camera as needed
+
+	// Draw game world to the window
+	_target.setView(camera);
+
+	//TODO
+
+	// Reset view
+	_target.setView(_target.getDefaultView());
+	
+
+}
+
+
+void Level::Update(sf::Time _frameTime)
+{
+	// TODO
+}
+
+
+void Level::LoadLevel(int _levelToLoad)
+{
+	// Clean up the old level
+
+	// Delete any data already in the level
+	//TODO
+
+	// Clear out our lists
+	// TODO
+
+
+
+	// Set the current level
+	m_currentLevel = _levelToLoad;
+
+
+	// Set up the new level
+
+	// Open our file for reading
+	std::ifstream inFile;
+	std::string fileName = "levels/Level" + std::to_string(m_currentLevel) + ".txt";
+	inFile.open(fileName);
+
+	// Make sure the file was opened
+	if (!inFile) {
+		std::cerr << "Unable to open file " + fileName;
+		exit(1); // Call system to stop program with error
+	}
+
+	// Set the starting x and y coordinates used to position level objects
+	float x = 0.0f;
+	float y = 0.0f;
+
+	// Define the spacing we will use for our grid
+	const float X_SPACE = 100.0f;
+	const float Y_SPACE = 100.0f;
+
+	// Create the player first as other objects will need to reference it
+	
+
+	// Read each character one by one from the file...
+	char ch;
+	// Each time, try to read the next character
+	// If successful, execute body of loop
+	// the "noskipws" means our input from the file will include 
+	// the white space (spaces and new lines)
+	while (inFile >> std::noskipws >> ch)
+	{
+		// Perform actions based on what character was read in
+
+		if (ch == ' ')
+		{
+			x += X_SPACE;
+		}
+		else if (ch == '\n')
+		{
+			y += Y_SPACE;
+			x = 0;
+		}
+		
+		else if (ch == '-')
+		{
+			// Do nothing - empty space
+		}
+		else
+		{
+			std::cerr << "Unrecognised character in level file: " << ch;
+		}
+	}
+
+	// Close the file now that we are done with it
+	inFile.close();
+
+
+
+}
+
+void Level::ReloadLevel()
+{
+	LoadLevel(m_currentLevel);
+}
+
+void Level::LoadNextLevel()
+{
+	LoadLevel(m_currentLevel + 1);
+}
---
+++ Level.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Library Includes
+#include <SFML/Graphics.hpp>
+
+class Level
+{
+
+public:
+
+	Level();
+
+	void Draw(sf::RenderTarget& _target);
+	void Update(sf::Time _frameTime);
+
+	void LoadLevel(int _levelToLoad);
+	void ReloadLevel();
+	void LoadNextLevel();
+
+private: 
+
+	int m_currentLevel;
+
+};
